5393337c0a38cfbc595ca2fe79fb929f64daf58c
ydario <ydario@60da5b81-1a84-de11-bd16-00e081727c95>
2010-05-10 17:09:46 +0000

    Refactoring source tree. ticket:3.

---
+++ trunk/src/plugin_RCDefines.h
@@ -0,0 +1,35 @@
+/* $Id: plugin_RCDefines.h,v 1.1 2003/09/14 22:37:31 bird Exp $
+ *
+ * Defines for the Plugin Resource File.
+ *
+ * InnoTek Systemberatung GmbH confidential
+ *
+ * Copyright (c) 2003 InnoTek Systemberatung GmbH
+ * Author: knut st. osmundsen <bird-srcspam@anduin.net>
+ *
+ * All Rights Reserved
+ *
+ */
+
+#ifndef __plugin_RCDefs_h__
+#define __plugin_RCDefs_h__
+
+/* RC_DATA types for version info - required */
+#define NP_INFO_ProductVersion      1
+#define NP_INFO_MIMEType            2
+#define NP_INFO_FileOpenName        3
+#define NP_INFO_FileExtents         4
+
+/* RC_DATA types for version info - used if found */
+#define NP_INFO_FileDescription     5
+#define NP_INFO_ProductName         6
+
+/* RC_DATA types for version info - optional */
+#define NP_INFO_CompanyName         7
+#define NP_INFO_FileVersion         8
+#define NP_INFO_InternalName        9
+#define NP_INFO_LegalCopyright      10
+#define NP_INFO_OriginalFilename    11
+
+#endif
+
