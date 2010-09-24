7db12e572a327d5de8abf02d99a1a3c43d1592c3
ydario <ydario@60da5b81-1a84-de11-bd16-00e081727c95>
2010-09-24 17:14:03 +0000

    Cleanup of old installation files.

--- trunk/install/flash10.cmd
+++
@@ -1,6 +0,0 @@
-
-del ..\bin\debug\flash10os2_debug.wpi
-wic.exe ..\bin\debug\flash10os2_debug.wpi -a 2 -c"..\bin\debug" npflos2.dll 2 flash10.txt flash10.lic 3 NPSWF32.DLL -s flash10os2_release.wis
-
-del ..\bin\debug\flash10os2_release.wpi
-wic.exe ..\bin\debug\flash10os2_release.wpi -a 2 -c"..\bin\release" npflos2.dll 2 flash10.txt flash10.lic 3 NPSWF32.DLL -s flash10os2_release.wis
