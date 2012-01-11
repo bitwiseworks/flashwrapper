08eb93a21c2b4cb1f810f96a0aa8cff1ceefc4c3
Dmitriy Kuminov <coding@dmik.org>
2012-01-11 18:37:03 +0000

    Cleanup env.tpl.cmd.

--- env.tpl.cmd
+++ env.tpl.cmd
@@ -1,6 +1,18 @@
 @echo off
-rem set KBUILD_VERBOSE=2
-rem init gcc env here, e.g:
+
+rem
+rem Initialize GCC environment. By default, it is assumed that GCC
+rem can be found in PATH.
+rem
 rem call gcc446.cmd
-rem BUILD_PLATFORM will confuse recent kBuilds
-set BUILD_PLATFORM=
+rem 
+rem Some GCC environment scripts may incorrectly set BUILD_PLATFORM
+rem to a value confusing recent kBuilds so that it needs to be reset.
+rem
+rem set BUILD_PLATFORM=
+
+rem
+rem Cause kBuild to print all executed commands.
+rem 
+rem set KBUILD_VERBOSE=2
+
