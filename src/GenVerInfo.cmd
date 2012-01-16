b4650e3a2fcc753a2a5cf071a9ae589d592acf4c
Dmitriy Kuminov <coding@dmik.org>
2012-01-16 18:03:39 +0000

    Restore the original plugin file description string.
    This is necessary for some silly JS scripts so that they can
    believe that the user has Flash of a certain version installed.

--- src/GenVerInfo.cmd
+++ src/GenVerInfo.cmd
@@ -39,5 +39,17 @@ mozBuild = build' ('aVersion')'
 
 call lineout aOutFile, 'RCDATA NP_INFO_ProductVersion { 'mozVersion' }'
 call lineout aOutFile, 'RCDATA NP_INFO_FileVersion { 'mozVersion' }'
-call lineout aOutFile, 'RCDATA NP_INFO_FileDescription { "Shockwave Flash Wrapper for OS/2'mozBuild'\0" }'
+
+/*
+ * NOTE: there is a JS script used on many sites to detect the Flash version
+ * (http://code.google.com/p/swfobject/) which uses a silly method of parsing
+ * the file description field for that. So we have to emulate the description
+ * string of the real Flash plugin to trick this script and make it happy.
+ *
+ * The proper description should have been this:
+ *
+ *   call lineout aOutFile, 'RCDATA NP_INFO_FileDescription { "Shockwave Flash Wrapper for OS/2'mozBuild'\0" }'
+ */
+call lineout aOutFile, 'RCDATA NP_INFO_FileDescription { "Shockwave Flash 10.0 r45\0" }'
+
 call lineout aOutFile
