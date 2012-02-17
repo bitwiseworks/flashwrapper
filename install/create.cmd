858acf8c8e8fae17de58ae5441e48b09951e42b5
Dmitriy Kuminov <coding@dmik.org>
2012-02-17 09:06:04 +0000

    Update readme and installer scripts to match Beta 1 release.

---
+++ install/create.cmd
@@ -0,0 +1,6 @@
+/* This is a script to start the WarpIn Archive Creator to create a WarpIn file */
+
+wic_dir = Strip(SysIni('user','WarpIN','Path'),'t','00'x)
+old = SysSetExtLIBPATH(wic_dir,'b')
+Address cmd wic_dir||'\wic.exe -a -U flash_ecs_beta1-release 12 -c12 -r * 13 -c13 -r * -s flash_ecs.wis'
+old = SysSetExtLIBPATH(old,'b')
