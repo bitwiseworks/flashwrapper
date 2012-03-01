6526c99049563d6f22697aede0b61d611608e646
dmik <dmik@60da5b81-1a84-de11-bd16-00e081727c95>
2012-03-01 15:45:30 +0000

    Remove 0.3.2 tag (will be recreated).

--- tags/0.3.2/src/BuildAll.cmd
+++
@@ -1,9 +0,0 @@
-REM *** VAC++ 3.6 build ***
-
-nmake BUILD_MODE=debug clean
-nmake BUILD_MODE=debug
-nmake BUILD_MODE=debug install 
-
-nmake BUILD_MODE=release clean
-nmake BUILD_MODE=release
-nmake BUILD_MODE=release install 
