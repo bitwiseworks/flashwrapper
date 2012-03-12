0ab6b9fd252ba95e660377b9434f318a7ae9cad1
Dmitriy Kuminov <coding@dmik.org>
2012-03-12 11:58:11 +0000

    Install files for 0.3.2.

--- install/create.cmd
+++ install/create.cmd
@@ -2,5 +2,5 @@
 
 wic_dir = Strip(SysIni('user','WarpIN','Path'),'t','00'x)
 old = SysSetExtLIBPATH(wic_dir,'b')
-Address cmd wic_dir||'\wic.exe -a -U flash_ecs_beta1-release 12 -c12 -r * 13 -c13 -r * -s flash_ecs.wis'
+Address cmd wic_dir||'\wic.exe -a -U flash_ecs_beta2-release 12 -c12 -r * 13 -c13 -r * -s flash_ecs.wis'
 old = SysSetExtLIBPATH(old,'b')
