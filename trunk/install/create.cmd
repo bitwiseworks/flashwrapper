18c2393517a3b5ffa07457a8f5fc587f7f61e194
dmik <dmik@60da5b81-1a84-de11-bd16-00e081727c95>
2012-12-19 17:42:05 +0000

    Release 0.4.0.

--- trunk/install/create.cmd
+++ trunk/install/create.cmd
@@ -1,6 +1,17 @@
 /* This is a script to start the WarpIn Archive Creator to create a WarpIn file */
 
+'@echo off'
+
+'mkdir 12'
+'xcopy eula.pdf 12\'
+'xcopy flash.lic 12\'
+'xcopy flash.txt 12\'
+'xcopy npflos2.dll 12\'
+
+'mkdir 13'
+'xcopy NPSWF32.dll 13\'
+
 wic_dir = Strip(SysIni('user','WarpIN','Path'),'t','00'x)
 old = SysSetExtLIBPATH(wic_dir,'b')
-Address cmd wic_dir||'\wic.exe -a -U flash_ecs_beta2-release 12 -c12 -r * 13 -c13 -r * -s flash_ecs.wis'
+Address cmd wic_dir||'\wic.exe -a -U flash_ecs_ga-release 12 -c12 -r * 13 -c13 -r * -s flash_ecs.wis'
 old = SysSetExtLIBPATH(old,'b')
